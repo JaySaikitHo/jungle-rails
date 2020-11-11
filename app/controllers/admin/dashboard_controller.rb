4ad7e23ecb02a79e6b3807714cf437395f4e4b85
vagrant <ttjho@hotmail.com>
2020-11-11 21:32:27 +0000

    add dashboard total product and categories count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+  @product_total = Product.sum("quantity")
+  
+  @product_categories = Category.select("id").uniq.length
+  # raise @product_categories.inspect
   end
 end
