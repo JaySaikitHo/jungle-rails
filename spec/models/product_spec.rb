dda0b89f3d5ca4fef0783e227bbba8f81b7872aa
vagrant <ttjho@hotmail.com>
2020-11-16 21:45:47 +0000

    Add product test for save, failing test for :name, :price, :quantity and :category

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+ 
+  describe 'Validations' do
+    it 'should save successfully' do
+      @product = Product.new
+      @product.name = 'Crap'
+      @product.price = 10
+      @product.quantity = 3
+      @product.category_id = 1
+
+      expect(@product).to be_present
+    end
+
+    it 'should have an error for the name' do
+      @product = Product.new
+      @product.name = nil
+      @product.price = 10
+      @product.quantity = 3
+      @product.category_id = 1
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+      
+    end
+  
+    it 'should have an error for the price' do
+      @product = Product.new
+      @product.name = 'Stuff'
+      @product.price = nil
+      @product.quantity = 3
+      @product.category_id = 1
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+      
+    end
+    
+    it 'should have an error for the quantity' do
+      @product = Product.new
+      @product.name = 'Stuff'
+      @product.price = 10
+      @product.quantity = nil
+      @product.category_id = 1
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      
+    end
+        
+    it 'should have an error for the category' do
+      @product = Product.new
+      @product.name = 'Stuff'
+      @product.price = 10
+      @product.quantity = 3
+      @product.category_id = nil
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+      
+    end
+  
+  end
+  
+    
+  
+
+end
